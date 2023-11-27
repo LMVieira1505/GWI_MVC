07b8d5fc63290fd2cfc3abb86d16e616a5dd070b
LMVieira1505 <133874629+LMVieira1505@users.noreply.github.com>
2023-11-27 12:25:24 -0300

    subcat

---
+++ Models/SubcatNoticia.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GWI.Models
+{
+    public class SubcatNoticia
+    {
+        public int sctnt_id { get; set; }
+
+        [Required(ErrorMessage = "Campo nome obrigatório", AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Mínimo de 3 e máximo de 100 caracteres.")]
+        public int sctnt_nt_id { get; set; }
+
+        public int sbtnt_sct_id { get; set; }
+
+        public SubcatNoticia()
+        {
+            sctnt_id = 0;
+            sctnt_nt_id = 0;
+            sbtnt_sct_id = 0;
+
+        }
+    }
+}
---
+++ Repositories/ADO/SQLServer/SubcatNoticia.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Data.SqlClient;
+using GWI.Models;
+
+namespace GWI.Repositories.ADO.SQLServer
+{
+    public class SubcatNoticiaADO
+    {
+        private readonly string connectionString; //Declarado para toda a classe. Possível alterar somente no construtor.
+        public SubcatNoticiaADO(string connectionString) //Quem invocar o construtor do repositório deve enviar a string de conexão.
+        {
+            this.connectionString = connectionString; //atualização do atributo por meio do valor que veio no parâmetro do construtor..
+        }
+
+        public List<Models.SubcatNoticia> GetAll()
+        {
+            List<Models.SubcatNoticia> SubcatNoticias = new List<Models.SubcatNoticia>();
+
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandText = "select sctnt_id, sctnt_nt_id, sbtnt_sct_id from tb_sctnt;";
+
+                    SqlDataReader dr = command.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        Models.SubcatNoticia SubcatNoticia = new Models.SubcatNoticia();
+                        SubcatNoticia.sctnt_id = (int)dr["cat_id"];
+                        SubcatNoticia.sctnt_nt_id = (int)dr["cat_id"];
+                        SubcatNoticia.sbtnt_sct_id = (int)dr["cat_id"];
+                        SubcatNoticias.Add(SubcatNoticia);
+                    }
+                }
+            }
+
+            return SubcatNoticias;
+        }
+
+    }
+}
